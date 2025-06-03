86b1ae7c54f10af8465db3c726f7869f903bffa3
Franklin Carrero <franklin.carrero@conekta.com>
2025-06-03 15:15:35 -0500

    feat: Add integration tests and GitHub Actions workflow (#4)
    * feat: Add integration tests and GitHub Actions workflow
    
    - Added integration tests for `CardForm` submission in `integration_test/card_form_test.dart`.
    - Created `MockPaymentService` in `integration_test/mocks/mock_payment_service.dart` for testing purposes.
    - Included `integration_test` as a dev dependency in `pubspec.yaml`.
    - Configured a GitHub Actions workflow in `.github/workflows/flutter_test.yml` to run unit, widget, and integration tests. This includes setting up Java, accepting Android licenses, and setting up an Android emulator.
    - Made minor formatting adjustments in `payment_service.dart` and `card_form.dart`.
    
    * Test: Configure GitHub Actions for integration tests
    
    - Updated `card_form_test.dart` to remove comments.
    - Modified `.github/workflows/flutter_test.yml` to:
        - Enable KVM for Android emulator.
        - Set Android SDK API level to 27 for integration tests.
        - Remove `target`, `arch`, and `profile` settings for Android emulator.
    - Added `/android/.kotlin` to `.gitignore`.
    
    * test: Suppress ElevatedButton tap warning in integration test
    
    - Added `warnIfMissed: false` to the `tester.tap` call for `ElevatedButton` in `card_form_test.dart` to suppress a potential warning if the widget is not found. This is likely due to the button being off-screen or obscured.
    
    * Adjust Android emulator resources for integration tests
    
    - Increased heap size to 1024M.
    - Enabled animations.
    - Set CPU cores to 8.
    - Increased RAM size to 12288M.
    
    * Adjust Android emulator resources for integration tests
    
    - Increased heap size to 1024M.
    - Enabled animations.
    - Set CPU cores to 8.
    - Increased RAM size to 12288M.
    
    * Adjust Android emulator resources for integration tests
    
    - Increased heap size to 1024M.
    - Enabled animations.
    - Set CPU cores to 8.
    - Increased RAM size to 12288M.
    
    * Fix: Remove profile parameter from Android emulator setup
    
    The `profile: pixel` parameter has been removed from the Android emulator setup in the GitHub Actions workflow. This change simplifies the emulator configuration.
    
    * parallel test
    
    * test: Update integration test and GitHub Actions workflow
    
    - Removed `warnIfMissed: true` from `ElevatedButton` tap in `card_form_test.dart`.
    - Changed `disable-animations` from `false` to `true` in `flutter_test.yml`.
    - Updated `.gitignore` to correctly ignore the `/android/.kotlin` directory.
    
    * Refactor: Format code for improved readability
    
    - Applied code formatting to `card_model.dart`, `theme.dart`, and `secure_payment_section_test.dart`.
    - Removed comments from `card_model.dart`.
    
    * feat: Enable web platform support
    
    - Added `web` to the list of supported platforms in `pubspec.yaml`.
    
    * style: Update iOS and Android preview images
    
    - Updated `imgs/android.png` with a new screenshot.
    - Updated `imgs/ios.png` with a new screenshot.

---
+++ integration_test/card_form_test.dart
@@ -0,0 +1,49 @@
+import 'package:conekta_component/card_input_flutter.dart';
+import 'package:conekta_component/l10n/app_localizations.dart';
+import 'package:conekta_component/src/fields/card_cvv_field.dart';
+import 'package:conekta_component/src/fields/card_expiry_fields.dart';
+import 'package:conekta_component/src/fields/card_name_field.dart';
+import 'package:conekta_component/src/fields/card_number_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'mocks/mock_payment_service.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('CardForm submits and returns success', (tester) async {
+    bool submitted = false;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: Scaffold(
+          body: CardForm(
+            paymentService: MockPaymentService(apiKey: "key_xxx"),
+            onSubmitted: (result) {
+              submitted = result is Success<Map<String, dynamic>>;
+            },
+          ),
+        ),
+      ),
+    );
+
+    await tester.enterText(find.byType(CardNameField), 'Juan Pérez');
+    await tester.enterText(find.byType(CardNumberField), '4242424242424242');
+    await tester.enterText(find.byType(CardExpiryFields), '12/25');
+    await tester.enterText(find.byType(CardCVVField), '123');
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
+
+    expect(submitted, isTrue);
+  });
+}
--- lib/src/card_form.dart
+++ lib/src/card_form.dart
@@ -1,13 +1,14 @@
 import 'package:conekta_component/l10n/app_localizations.dart';
 import 'package:conekta_component/src/services/result.dart';
 import 'package:flutter/material.dart';
-import 'models/card_model.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 import 'fields/card_cvv_field.dart';
 import 'fields/card_expiry_fields.dart';
-import 'fields/card_number_field.dart';
 import 'fields/card_name_field.dart';
+import 'fields/card_number_field.dart';
+import 'models/card_model.dart';
 import 'services/payment_service.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'utils/theme.dart';
 import 'widgets/secure_payment_section.dart';
 
@@ -70,10 +71,11 @@ class _CardFormState extends State<CardForm> {
       );
 
       try {
-        final result = await widget.paymentService.sendPayment(card, widget.locale.languageCode);
+        final result = await widget.paymentService
+            .sendPayment(card, widget.locale.languageCode);
         cleanFields();
         widget.onSubmitted?.call(Success(result));
-      } on Exception catch (  e ) {
+      } on Exception catch (e) {
         widget.onSubmitted?.call(Failure(e));
       } finally {
         setState(() {
@@ -82,7 +84,8 @@ class _CardFormState extends State<CardForm> {
       }
     }
   }
-  cleanFields(){
+
+  cleanFields() {
     cardNumberController.clear();
     nameController.clear();
     expiryDateController.clear();
